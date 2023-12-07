e3e49317b5a594d6754f93e54b542c7b301aa072
“tsingbochun” <tsingbochun@gmail.com>
2023-12-07 13:45:55 +0800

    Q3: Find the Bug finished
    Q3: Find the Bug finished

--- disc3.py
+++ disc3.py
@@ -7,4 +7,21 @@ def multiply(m, n):
     if n == 1:
         return m * 1
     else:
-        return m + multiply(m, n-1)
+        return m + multiply(m, n-1)  # Q1: Warm Up: Recursive Multiplication finished
+    
+# Q3: Find the Bug
+# Find the bug in this recursive function.
+def skip_mul(n):
+    """Return the product of n * (n - 2) * (n - 4) * ...
+
+    >>> skip_mul(5) # 5 * 3 * 1
+    15
+    >>> skip_mul(8) # 8 * 6 * 4 * 2
+    384
+    """
+    if n == 2:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return n * skip_mul(n - 2)
