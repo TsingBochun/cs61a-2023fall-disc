da93bf78fbaac7a9cd0074620e857774e4bfeb25
“tsingbochun” <tsingbochun@gmail.com>
2023-12-07 13:41:03 +0800

    Q1: Warm Up: Recursive Multiplication finished
    Q1: Warm Up: Recursive Multiplication finished

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cs61a-2023fall-disc
- cs61a-2023fall-disc
+- cs61a-2023fall-disc
+- 本仓库用来存放我个人写的一些CS61A 2023FALL disc上的一些代码
---
+++ disc3.py
@@ -0,0 +1,10 @@
+def multiply(m, n):
+    """Takes two positive integers and returns their product using recursion.
+    >>> multiply(5, 3)
+    15
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return m * 1
+    else:
+        return m + multiply(m, n-1)
