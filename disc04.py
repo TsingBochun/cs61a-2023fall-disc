fb4359d6d4ccdf75475dc7095ad3ee3d0d3c06d8
“tsingbochun” <tsingbochun@gmail.com>
2023-12-09 15:32:28 +0800

    Q1: Count Stair Ways skip
    Q1: Count Stair Ways skip 想不出来，暂时跳过去

---
+++ disc04.py
@@ -0,0 +1,28 @@
+# This is practise of disc04.
+
+# Q1: Count Stair Ways
+def count_stair_ways(n):
+    """Returns the number of ways to climb up a flight of
+    n stairs, moving either one step or two steps at a time.
+    >>> count_stair_ways(1)
+    1
+    >>> count_stair_ways(2)
+    2
+    >>> count_stair_ways(4)
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    #result = 0
+    #if n == 1:         # 台阶只有一个，那么一次迈一个，只有一种方法
+    #    result =  1
+    #elif n == 2:
+    #    result =  2      # 台阶有两个，那么一次迈一个迈两次，或者一次迈两个，有两种方法
+    #else:
+        #result_last1 = count_stair_ways(n-1) + 1       # 计算当最后一步只需迈一个的时候方法
+        # result_last2 = count_stair_ways(n-2)       # 计算当最后一步为两个台阶的方法
+        #result = result_last2 + count_stair_ways(2)
+        #result = count_stair_ways(n-2) + count_stair_ways(2) 
+    #return result
+    # 问题本身相当于将n按照不大于2进行分割有多少种方法：           # skip
+    
+    
--- disc3.py
+++ disc3.py
@@ -21,7 +21,64 @@ def skip_mul(n):
     """
     if n == 2:
         return 2
-    elif n == 1:
-        return 1
+    elif n == 1:          # added by myself
+        return 1           # added by myself       # Q3: Find the Bug finished
     else:
         return n * skip_mul(n - 2)
+    
+# Q4: Is Prime
+# Write a function is_prime that takes a single argument n and 
+# returns True if n is a prime number and False otherwise. 
+# Assume n > 1. We implemented this in Discussion 1 iteratively, now time to do it recursively!
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    "*** YOUR CODE HERE ***"        # skip
+    def check(i):
+        "Check whether no number from i up to n evenly divides n."
+        if i == n:
+            return True
+        elif n % i == 0:
+            return False
+        return check(i + 1)
+    return check(2)
+
+
+# Q5: Recursive Hailstone
+# Recall the hailstone function from Homework 1. 
+# First, pick a positive integer n as the start. 
+# If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1. #偶数就除以2，奇数就乘以三再加1
+# Repeat this process until n is 1.             # 重复执行，直到n等于1
+# Write a recursive version of hailstone that prints out the values of the sequence and returns the number of steps.
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the number of elements in the sequence.
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    print(n)
+    if n == 1:
+        return 1
+    elif n % 2 == 0:
+        return 1 + hailstone(n // 2)
+    else:
+        return 1 + hailstone(3 * n + 1)
+
