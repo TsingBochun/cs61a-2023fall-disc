0d1bcfb9bc6c0a18f2e9fe4ea9a6722c14f83057
“tsingbochun” <tsingbochun@gmail.com>
2023-12-16 15:26:20 +0800

    Q3: Less Than FINISHED
    Q3: Less Than FINISHED

--- disc05.py
+++ disc05.py
@@ -41,4 +41,33 @@ def div_rat(x, y):
     result[0] = numer(x) * denom(y)
     # 分母是，X的分母，乘以Y的分子
     result[1] = denom(x) * numer(y)
-    return result
+    return result                    # Q2: Divide FINISHED
+
+
+# Q3: Less Than
+def lt_rat(x, y):
+    """Returns True iff x < y as rational numbers; else False.
+    >>> a, b = make_rat(6, 7), make_rat(12, 16)
+    >>> lt_rat(a, b)
+    False
+    >>> lt_rat(b, a)
+    True
+    >>> lt_rat(a, b)
+    False
+    >>> a, b = make_rat(-6, 7), make_rat(-12, 16)
+    >>> lt_rat(a, b)
+    True
+    >>> lt_rat(b, a)
+    False
+    >>> lt_rat(a, a)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    x_0 = denom(x)    # x 的分母
+    x_1 = numer(x)    # x 的分子
+    y_0 = denom(y)    # y 的分母
+    y_1 = numer(y)    # y 的分子
+    result = (x_1 * y_0 - x_0 * y_1) / x_0 * y_0
+    if result < 0:
+        return True
+    return False
