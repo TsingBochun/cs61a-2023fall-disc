09755108c3d9c7b98084efa2ffd70e90833825e4
“tsingbochun” <tsingbochun@gmail.com>
2023-12-19 13:08:40 +0800

    Q3: Filter-Iter FINISHED
    Q3: Filter-Iter FINISHED

--- disc06.py
+++ disc06.py
@@ -1,2 +1,23 @@
 # THIS IS DISC06
 
+# Q3: Filter-Iter
+def filter_iter(iterable, f):
+    """
+    Generates elements of iterable for which f returns True.
+    >>> is_even = lambda x: x % 2 == 0
+    >>> list(filter_iter(range(5), is_even)) # a list of the values yielded from the call to filter_iter
+    [0, 2, 4]
+    >>> all_odd = (2*y-1 for y in range(5))
+    >>> list(filter_iter(all_odd, is_even))
+    []
+    >>> naturals = (n for n in range(1, 100))
+    >>> s = filter_iter(naturals, is_even)
+    >>> next(s)
+    2
+    >>> next(s)
+    4
+    """
+    "*** YOUR CODE HERE ***"
+    for element in iterable:
+        if f(element):
+            yield element 
