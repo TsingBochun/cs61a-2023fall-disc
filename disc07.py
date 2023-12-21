537ec3ee947541360c6bc4eb8a1443660bae1a7a
“tsingbochun” <tsingbochun@gmail.com>
2023-12-21 16:55:37 +0800

    Q6: NoisyCat FINISHED
    Q6: NoisyCat FINISHED

--- disc07.py
+++ disc07.py
@@ -135,7 +135,87 @@ class MimicButterfly(Monarch):
     #    _______________.__init__()
         super().__init__()
         #______________ = mimic_animal
-        self.mimic_animal = mimic_animal
+        self.mimic_animal = mimic_animal     # Q4: That's inheritance, init? FINISHED
+
+# Q5: Cat
+class Pet():
+
+    def __init__(self, name, owner):
+        self.is_alive = True    # It's alive!!!
+        self.name = name
+        self.owner = owner
+
+    def eat(self, thing):
+        print(self.name + " ate a " + str(thing) + "!")
+
+    def talk(self):
+        print(self.name)
+
+class Cat(Pet):
+
+    def __init__(self, name, owner, lives=9):
+        "*** YOUR CODE HERE ***"
+        super().__init__(name, owner)
+        #self.name = name
+        #self.owner = owner
+        self.lives = lives
+
+    def talk(self):
+        """Print out a cat's greeting.
+
+        >>> Cat('Thomas', 'Tammy').talk()
+        Thomas says meow!
+        """
+        "*** YOUR CODE HERE ***"    
+        print(self.name + " says meow!")
+
+    def lose_life(self):
+        """Decrements a cat's life by 1. When lives reaches zero,
+        is_alive becomes False. If this is called after lives has
+        reached zero, print 'This cat has no more lives to lose.'
+        """
+        "*** YOUR CODE HERE ***"
+        if self.lives > 0:
+            self.lives -= 1
+        else:
+            self.is_alive = False
+            print("This cat has no more lives to lose.")
+
+    def revive(self):
+        """Revives a cat from the dead. The cat should now have 
+        9 lives and is_alive should be true. Can only be called
+        on a cat that is dead. If the cat isn't dead, print 
+        'This cat still has lives to lose.'
+        """
+        if not self.is_alive:
+            #____________________________
+            self.lives = 9
+            self.is_alive = True
+        else:
+            #____________________________
+            print("This cat still has lives to lose.")
+
+# Q6: NoisyCat
+# class __________ # Fill me in!
+class NoisyCat(Cat):
+    """A Cat that repeats things twice."""
+    def __init__(self, name, owner, lives=9):
+        # Is this method necessary? Why or why not?
+        "*** YOUR CODE HERE ***"
+        super().__init__(name, owner, lives)
+
+    def talk(self):
+        """Talks twice as much as a regular cat.
+        >>> NoisyCat('Magic', 'James').talk()
+        Magic says meow!
+        Magic says meow!
+        """
+        "*** YOUR CODE HERE ***"
+        for _ in range(2):
+            print(self.name + " says meow!")
+
+
+    
 
 
 
