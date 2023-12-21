885279941514c58069fafff8b0f1fde25278a800
“tsingbochun” <tsingbochun@gmail.com>
2023-12-21 10:30:40 +0800

    Q2: Email class finished
    Q2: Email class finished

--- disc07.py
+++ disc07.py
@@ -27,4 +27,23 @@ class Professor:
         student.understanding += 1
 
     def grant_more_extension_days(self, student, days):
-        student.extension_days = days
+        student.extension_days = days      # Q1 WWPD FINISHED
+
+# Q2: Email
+class Email:
+    """
+    Every email object has 3 instance attributes: the
+    message, the sender name, and the recipient name.
+    >>> email = Email('hello', 'Alice', 'Bob')
+    >>> email.msg
+    'hello'
+    >>> email.sender_name
+    'Alice'
+    >>> email.recipient_name
+    'Bob'
+    """
+    def __init__(self, msg, sender_name, recipient_name):
+        "*** YOUR CODE HERE ***"
+        self.msg = msg
+        self.sender_name = sender_name
+        self.recipient_name = recipient_name
