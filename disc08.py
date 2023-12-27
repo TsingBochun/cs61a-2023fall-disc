3ccd33e5e85bdfd8f09894f23b50caccf246f95d
“tsingbochun” <tsingbochun@gmail.com>
2023-12-27 14:02:13 +0800

    Q2: Sum Nums FINISHED
    Q2: Sum Nums FINISHED

--- disc08.py
+++ disc08.py
@@ -21,7 +21,27 @@ class Link:
         while self.rest is not Link.empty:
             string += str(self.first) + ''
             self = self.rest
-        return string + str(self.first) + '>'
+        return string + str(self.first) + '>'       # Linked List ADT FINISHED and Q1 FINISHED
+    
+# Q2: Sum Nums
+def sum_nums(s):
+    """
+    Returns the sum of the elements in s.
+
+    >>> a = Link(1, Link(6, Link(7)))
+    >>> sum_nums(a)
+    14
+    """
+    "*** YOUR CODE HERE ***"
+    result = 0
+    if s is Link.empty:
+        return ()
+    elif s.rest is Link.empty:
+        result += s.first
+        return result
+    else:
+        result = s.first + sum_nums(s.rest)
+        return result                             # Q2: Sum Nums FINISHED
     
 
         
