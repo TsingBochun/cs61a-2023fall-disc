aa88cd918a91d552cf217b5540d71526a2b7e63b
“tsingbochun” <tsingbochun@gmail.com>
2023-12-27 15:23:22 +0800

    Q3: Remove All SKIPPED
    Q3: Remove All SKIPPED：暂时有一个BUG始终无法解决不了，先跳过

--- disc08.py
+++ disc08.py
@@ -19,7 +19,7 @@ class Link:
     def __str__(self):
         string = '<'
         while self.rest is not Link.empty:
-            string += str(self.first) + ''
+            string += str(self.first) + ' '
             self = self.rest
         return string + str(self.first) + '>'       # Linked List ADT FINISHED and Q1 FINISHED
     
@@ -43,6 +43,52 @@ def sum_nums(s):
         result = s.first + sum_nums(s.rest)
         return result                             # Q2: Sum Nums FINISHED
     
+
+# Q3: Remove All
+def remove_all(link, value):
+    """Removes all nodes in link that contain value. The first element of
+    link is never equal to value.
+
+    >>> l1 = Link(0, Link(2, Link(2, Link(3, Link(1, Link(2, Link(3)))))))
+    >>> print(l1)
+    <0 2 2 3 1 2 3>
+    >>> remove_all(l1, 2)
+    >>> print(l1)
+    <0 3 1 3>
+    >>> remove_all(l1, 3)
+    >>> print(l1)
+    <0 1>
+    >>> remove_all(l1, 3)
+    >>> print(l1)
+    <0 1>
+    """
+    "*** YOUR CODE HERE ***"
+    remain = link.rest
+    if remain is Link.empty:
+        return 
+    else:
+        while True:
+            if remain.first == value:
+                remain = remain.rest
+            else:
+                link.rest = remain
+                remain = 
+        #    link.rest = remain.rest
+            link.rest = remain.rest
+            print(link)   # only for debug
+    #remove_all(link.rest, value)
+    #if link.rest is link.empty:
+    #    return
+    #elif link.rest.first == value:
+    #    link.rest = link.rest.rest
+    #remove_all(link, value)
+
+
+# Q5: Make Circular
+
+    
+    
+
 
         
         
