756fc151d6cc85d66e2740207bad58345ebfb595
“tsingbochun” <tsingbochun@gmail.com>
2023-12-27 13:47:15 +0800

    Linked List ADT FINISHED and Q1 FINISHED
    Linked List ADT FINISHED and Q1 FINISHED

--- disc08.py
+++ disc08.py
@@ -1,1 +1,28 @@
 # THIS IS DISC08
+
+# linked list ADT
+class Link:
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)     # 断言剩余要么是空表，要么是一个LINK
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+    
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ''
+            self = self.rest
+        return string + str(self.first) + '>'
+    
+
+        
+        
