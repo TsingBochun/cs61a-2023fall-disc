29b3653a47c04c0658b5baa52dc5ae06c4b8edf7
“tsingbochun” <tsingbochun@gmail.com>
2023-12-28 16:19:40 +0800

    Q4: Level Mutation Link 大致看懂了
    Q4: Level Mutation Link 大致看懂了

--- disc09.py
+++ disc09.py
@@ -54,4 +54,68 @@ def widest_level(t):
         levels.append([t.label for t in x])
         x = sum([t.branches for t in x], [])       # Q3: Widest Level 完全没看懂
     return max(levels, key=len)
+
+
+# Q4: Level Mutation Link
+def level_mutation_link(t, funcs):
+	"""Mutates t using the functions in the linked list funcs.
+
+	>>> t = Tree(1, [Tree(2, [Tree(3)])])
+	>>> funcs = Link(lambda x: x + 1, Link(lambda y: y * 5, Link(lambda z: z ** 2)))
+	>>> level_mutation_link(t, funcs)
+	>>> t
+	Tree(2, [Tree(10, [Tree(9)])])
+	>>> t2 = Tree(1, [Tree(2), Tree(3, [Tree(4)])])
+	>>> level_mutation_link(t2, funcs)
+	>>> t2
+	Tree(2, [Tree(100), Tree(15, [Tree(16)])])
+	>>> t3 = Tree(1, [Tree(2)])
+	>>> level_mutation_link(t3, funcs)
+	>>> t3
+	Tree(2, [Tree(100)])
+	"""
+	#if _____________________:
+#		return
+	#t.label = _____________________
+	#remaining = _____________________
+	#if __________________ and __________________:
+	#	while _____________________:
+	#		_____________________
+	#		remaining = remaining.rest
+	#for b in t.branches:
+	#	_____________________
+	if funcs is Link.empty:
+		return
+	t.label = funcs.first(t.label)
+	remaining = funcs.rest
+	if t.is_leaf() and remaining is not Link.empty:
+		while remaining is not Link.empty:
+			t.label = remaining.first(t.label)
+			remaining = remaining.rest
+	for b in t.branches:
+		level_mutation_link(b, remaining)     # Q4: Level Mutation Link 大致看懂了
+
+# linked list API
+class Link:
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)     # 断言剩余要么是空表，要么是一个LINK
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+    
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>' 
+    
     
