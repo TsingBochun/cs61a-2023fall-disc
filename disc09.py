c1b0a0637315205db9ded74e1d76dad88367f598
“tsingbochun” <tsingbochun@gmail.com>
2023-12-28 19:16:00 +0800

    Q8: Yield, Fibonacci!
    Q8: Yield, Fibonacci!

--- disc09.py
+++ disc09.py
@@ -179,5 +179,30 @@ def lgk_pow(n,k):
 def square(x):
      return x * x         # Q7 finished
 
+# Q8: Yield, Fibonacci!
+def fibs(f):
+    """Yield all Fibonacci numbers x for which f(x) is a true value.
+    >>> odds = fibs(lambda x: x % 2 == 1)
+    >>> [next(odds) for i in range(10)]
+    [1, 1, 3, 5, 13, 21, 55, 89, 233, 377]
+    >>> bigs = fibs(lambda x: x > 20)
+    >>> [next(bigs) for i in range(10)]
+    [21, 34, 55, 89, 144, 233, 377, 610, 987, 1597]
+    >>> evens = fibs(lambda x: x % 2 == 0)
+    >>> [next(evens) for i in range(10)]
+    [0, 2, 8, 34, 144, 610, 2584, 10946, 46368, 196418]
+    """
+    n, m = 0, 1
+#    while _________:
+#        if _________:
+#            _________
+#        n, m = _________, _________
+    while True:
+        if f(n):
+            yield n
+        n, m = m, n + m           # Q8: Yield, Fibonacci! 
+             
+    
+
 
     
